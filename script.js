4c37acbe639102d9cdde562e7395d047d323e222
coolername12345x123 <maroua546lol@gmail.com>
2026-05-07 00:07:24 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -58,7 +58,7 @@ function moveBall(selection) {
         b = randomIntFromInterval(5, 15);
     size = Math.min(a, b); 
   } else {
-    size = randomIntFromInterval(10, 30);
+    size = randomIntFromInterval(15, 50);
   }
 
   $(selection).css({ left: left+"%", top: top+"%", height: size+"vmin", width: size+"vmin" });
